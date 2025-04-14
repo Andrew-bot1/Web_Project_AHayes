1e103db7d804490ecf32a517d299fa7583c425fe
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-04-13 21:30:37 -0500

    Case 4

--- project_AHayes/java.js
+++ project_AHayes/java.js
@@ -2,8 +2,7 @@
 const date = new Date();
 
 //get second p tag for error message
-const p = document.getElementByTag[2]("div");
-
+let p = document.getElementsByTagName("div")[1];
 
 //get user date
 let checkinDate = new Date(document.getElementById("myStart").value);
@@ -12,16 +11,32 @@ try {
     if(checkinDate.getDay()<date.getDay()) throw "Your check in date can't be in the past";
 } catch (error){
     //add paragraph containing error if the if statment is correct
-    p.insertAdjacentHTML("afterend", "<p>"+ error + "</p>");
+    p.innerHTML=("afterend", "<p>"+ error + "</p>");
 }
-
-
-
-
-
 
 //check to see if date is not in winter
 if (date.getMonth !== 11 || date.getMonth !== 12 || date.getMonth !== 1) {
     //give window alert to let user know they cant submit the form
     window.alert("We only accpet applications in winter (November, December, January).");
+}
+
+
+//case 4
+//get email from user
+let email = document.getElementById("myEmail").value;
+//verification variable
+let verify = /@gmail.com$/g;
+
+//get id of where to insert submit button after
+let insert = document.getElementById("comments");
+
+//see if email contains domain
+if (verify.test(email)) {
+    //display error message
+    insert.insertAdjacentHTML("afterend","Email can't be from gmail").style.color="red";
+}
+else
+{
+    //allow user to submit
+    insert.insertAdjacentHTML("afterend","<label for='submit'> </label><input id='submit' type='submit' value='Apply Now'>");
 }
