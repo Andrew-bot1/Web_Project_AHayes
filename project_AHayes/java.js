9b128984683bc8873fd05a47ef169da9117794b6
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-04-21 00:39:21 -0500

    Update java.js

--- project_AHayes/java.js
+++ project_AHayes/java.js
@@ -1,3 +1,10 @@
+//get user object
+console.log(user);
+
+//booleans that will let user submit if true
+let emailBool=false;
+let nameBool=false;
+
 //get current date
 const date = new Date();
 
@@ -23,20 +30,59 @@ if (date.getMonth !== 11 || date.getMonth !== 12 || date.getMonth !== 1) {
 
 //case 4
 //get email from user
-let email = document.getElementById("myEmail").value;
+user.email; //changed this so it gets it from new object file
+
 //verification variable
 let verify = /@gmail.com$/g;
 
 //get id of where to insert submit button after
 let insert = document.getElementById("comments");
 
-//see if email contains domain
-if (verify.test(email)) {
+email.onchange=function(){
+    //see if email contains domain
+if (verify.test(email.value)) {
     //display error message
     insert.insertAdjacentHTML("afterend","Email can't be from gmail").style.color="red";
 }
 else
 {
-    //allow user to submit
+    //set bool to true
+    emailBool=true;
+}
+}
+
+
+
+//case 5
+//get budget
+budget=user.budget;
+//select room based on budget
+if (budget==100) {
+    getElementById("myRoom").value=100
+}
+else {
+    getElementById("myRoom").value=50;
+}
+
+//get name
+let name =user.name;
+
+//verification rule to see if there are any numbers
+let numVerify=/\d/g;
+
+email.onchange=function(){
+    //see if name has numbers
+if (numVerify.test(email.value)) {
+    //display error message
+    insert.insertAdjacentHTML("afterend","Name can't have number").style.color="red";
+}
+else {
+    //set bool to true
+    nameBool=true;
+}
+}
+
+//see if user can submit
+if (emailBool&&nameBool==true) {
     insert.insertAdjacentHTML("afterend","<label for='submit'> </label><input id='submit' type='submit' value='Apply Now'>");
 }
