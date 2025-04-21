a9b62bf497c6c668be271e7951cd0729c8111a74
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-04-21 00:38:46 -0500

    Add files via upload

---
+++ project_AHayes/objects.js
@@ -0,0 +1,44 @@
+//class for user
+class Users{
+    //user constructor
+    User(name,email,budget) {
+        this.name=name;
+        this.email=email;
+        this.budget=budget;
+    }
+}
+
+//class for room
+class Rooms {
+    //room constructor
+    Room (name,type,price) {
+        this.name=name;
+        this.type=type;
+        this.price=price;
+    }
+}
+
+//create objects for rooms
+const room1 = new Rooms("Cozy Den","Standard",50);
+const room2 = new Rooms("Mountain Peak","Deluxe",100);
+
+//get name
+let name=document.getElementById("myName").onchange = function(){
+    let name=this.value;
+}
+
+//get email
+let email=document.getElementById("myEmail").onchange = function(){
+    let email=this.value;
+}
+
+//get budget
+let budget=document.getElementById("mybudget").onchange = function(){
+    let budget=this.value;
+}
+
+//create user object
+let user = new Users(name,email,budget);
+
+//store objects in session storage
+sessionStorage.setItem("user",user);
